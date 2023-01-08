9994a43b4a3ed9983f5315be210298f9ae29c82c
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-01-08 13:34:14 +0200

    Added moveSaga to redux-saga

--- src/store/sagas/index.ts
+++ src/store/sagas/index.ts
@@ -2,13 +2,63 @@
 // when we actually start our implementation 
 // of the snake game.
 
+import {
+  CallEffect,
+  delay,
+  put,
+  PutEffect,
+  takeLatest
+} from "redux-saga/effects";
+import {
+  DOWN,
+  ISnakeCoord,
+  LEFT,
+  MOVE_DOWN,
+  MOVE_LEFT,
+  MOVE_RIGHT,
+  MOVE_UP, RIGHT,
+  setDisDirection, UP
+} from "../actions/index.tsx";
 
-import { all, fork } from 'redux-saga/effects';
+export function* moveSaga(params: {
+  type: string;
+  payload: ISnakeCoord;
+}): Generator<
+  | PutEffect<{ type: string; payload: ISnakeCoord }>
+  | PutEffect<{ type: string; payload: string }>
+  | CallEffect<true>
+> {
+  while (true) {
+    yield put({
+      type: params.type.split("_")[1],
+      payload: params.payload,
+    });
+    switch (params.type.split("_")[1]) {
+      case RIGHT:
+        yield put(setDisDirection(LEFT));
+        break;
 
+      case LEFT:
+        yield put(setDisDirection(RIGHT));
+        break;
 
-function* rootSaga() {
-  yield all([ ]);
+      case UP:
+        yield put(setDisDirection(DOWN));
+        break;
+
+      case DOWN:
+        yield put(setDisDirection(UP));
+        break;
+    }
+    yield delay(100);
+  }
 }
 
+function* watcherSagas() {
+  yield takeLatest(
+    [MOVE_RIGHT, MOVE_LEFT, MOVE_UP, MOVE_DOWN],
+    moveSaga
+  );
+}
 
-export default rootSaga;
+export default watcherSagas;
